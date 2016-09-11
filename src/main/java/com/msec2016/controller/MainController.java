cf5101dd3b26cb51f99939d4e6f0dac7a965f0fe
miaodx <953900030@qq.com>
2016-09-11 15:26:51 +0800

    miaodx;增加一些 Restful request

--- src/main/java/com/msec2016/controller/MainController.java
+++ src/main/java/com/msec2016/controller/MainController.java
@@ -3,17 +3,20 @@ import com.msec2016.model.Problem;
 import com.msec2016.service.ProblemGeneratorAndSolver;
 import com.msec2016.test.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by miao on 2016/9/6.
  */
 @Controller
 public class MainController {
+    Logger logger = Logger.getLogger("MainController");
 
     ProblemGeneratorAndSolver pgas =  ProblemGeneratorAndSolver.INSTANCE;
 
@@ -24,7 +27,7 @@ public class MainController {
 
     @RequestMapping("user")
     @ResponseBody
-    public Object returnUser() {
+    public User returnUser() {
         User miao = new User("miaodx",21);
         return miao;
     }
@@ -50,9 +53,9 @@ public class MainController {
     }
 
 
-    @RequestMapping("newProblems")
+    @RequestMapping(value = "newProblems")
     @ResponseBody
-    public Object returnNewProblems() {
+    public List<Problem> returnNewProblems() {
 
         List<Problem> ps = pgas.newProblems(10);
         pgas.solveAll(ps);
@@ -69,5 +72,23 @@ public class MainController {
         pgas.solveAll(ps);
 
         return ps;
+    }
+
+    @RequestMapping(value="/newUser/{username}/{age}",method=RequestMethod.GET)
+    @ResponseBody
+    public User show(@PathVariable String username,@PathVariable int age) {
+        logger.info("newUser got " + username + "," + age);
+        return  new User(username,age);
+    }
+
+    @RequestMapping(value="/newUserOlder/{username}/{age}",method=RequestMethod.GET)
+    @ResponseBody
+    public User show2(@PathVariable String username,@PathVariable String age) {
+        return  new User(username,Integer.parseInt(age+1));
+    }
+}
+@Controller class UrlNotFoundController {
+    @RequestMapping("*") public String test404(){
+            return "error";
     }
 }
